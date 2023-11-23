17830a5a24f8497e958f8f81dddb6e1ab9d7d021
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-11-22 21:17:49 -0300

    construindo a logica de setar as cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -54,6 +54,31 @@ const cardData = [
     },
 ];
 
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", ".src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
+
+    if(fieldSide===playerSides.player1) {
+        cardImage.addEventListener("click", ()=> {
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+    }
+
+    cardImage.addEventListener("mouseover", () => {
+        drawSelectCard(IdCard);
+    });
+
+    return cardImage;
+}
+
 async function drawCards(cardNumbers, fieldSide) {
     for(let i=0; i<cardNumbers; i++) {
         const randamIdCard = await getRandomCardId();

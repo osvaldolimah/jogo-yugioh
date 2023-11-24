7b638cd49dffbbadf2edc8fb31938db93ac8c02a
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-11-23 22:11:08 -0300

    ajustando o caminho dos recursos

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,11 +19,11 @@ const state ={
 };
 
 const playerSides = {
-    player1: "player-field-card",
-    computer: "computer-field-card",
+    player1: "player-cards",
+    computer: "computer-cards",
 };
 
-const pathImages = ".src/assets/icons/";
+const pathImages = "./src/assets/icons/";
 
 const cardData = [
     {
@@ -62,7 +62,7 @@ async function getRandomCardId() {
 async function createCardImage(IdCard, fieldSide) {
     const cardImage = document.createElement("img");
     cardImage.setAttribute("height", "100px");
-    cardImage.setAttribute("src", ".src/assets/icons/card-back.png");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
     cardImage.setAttribute("data-id", IdCard);
     cardImage.classList.add("card");
 

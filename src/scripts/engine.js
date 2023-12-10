da0697241d0aecc6e0229646d8364d5b25fd5330
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-12-10 15:10:59 -0300

    refatorando o codigo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -95,17 +95,40 @@ async function setCardsField(cardId) {
 
     let computerCardId = await getRandomCardId();
 
-    state.fieldCards.player.style.display = "block";
-    state.fieldCards.computer.style.display = "block";
+    await showHiddenCardFieldsImages(true);
 
-    state.fieldCards.player.src = cardData[cardId].img;
-    state.fieldCards.computer.src = cardData[computerCardId].img;
+    
+
+    await hiddenCarddetails();
+
+    await drawCardsInField(cardId, computerCardId);
 
     let duelResults = await checkDuelResults(cardId, computerCardId);
 
     await updateScore();
     await drawButton(duelResults);
 };
+
+    async function drawCardsInField(cardId, computerCardId) {
+        state.fieldCards.player.src = cardData[cardId].img;
+        state.fieldCards.computer.src = cardData[computerCardId].img;
+    };
+
+    async function showHiddenCardFieldsImages(value) {
+        if(value === true) {
+            state.fieldCards.player.style.display = "block";
+            state.fieldCards.computer.style.display = "block";
+        } else {
+            state.fieldCards.player.style.display = 'none';
+            state.fieldCards.computer.style.display = 'none';
+        }
+    }
+
+    async function hiddenCarddetails() {
+        state.cardSprites.avatar.src = "";
+        state.cardSprites.name.innerText = "";
+        state.cardSprites.type.innerText = "";
+    }
 
     async function drawButton(text) {
         state.actions.button.innerText = text.toUpperCase();
@@ -176,6 +199,8 @@ async function playAudio(status) {
 }
 
 function init () {
+    showHiddenCardFieldsImages(false);
+
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
 }
